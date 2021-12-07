88ec5393e23ca2c81a8bff10f104aa8f124aeb16
Evan <whospect@gmail.com>
2021-12-07 21:33:00 +0300

    CORS добавлен

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module TodoApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end  
+    end
   end
 end
